66972878772412899b38f2fc51d6afd1fe58c0db
JackSprat <jacksprat2k5@aol.com>
2016-07-08 01:49:38 +0100

    Readding %arg<n> parsing for kcommand

--- src/processing/command/CommandCommandView.java
+++ src/processing/command/CommandCommandView.java
@@ -33,14 +33,29 @@ public class CommandCommandView extends CommandBase {
 		
 		String commandStr = ((ProcCommand)parent).getCommand(getToken("alias").substring(1), getUser());
 		
+		String[] args = getToken("...").split(" ");
+		String[] commandSplitStr = commandStr.split("¦");
+		
+		for (String t : commandSplitStr) {
+			if (t.contains("%arg")) {
+				for (int argCounter = 1; argCounter < args.length; argCounter++) {
+					t = t.replace("%arg" + argCounter, args[argCounter]);
+				}
+				if (t.contains("%arg")) {
+					t = "";
+				}
+			}
+		}
+		
 		commandStr = commandStr.replaceAll("%me", getUser());
+		
 		return listOut.add(new OutgoingMessage(OutType.CHAT, commandStr, parent.channel));
 		
 	}
 
 	@Override public String getPermissionString() 			{ return "command.view"; }
 	@Override public PermissionClass getPermissionClass() 	{ return PermissionClass.User; }
-	@Override public String getFormatString() 				{ return ":<alias>"; }
+	@Override public String getFormatString() 				{ return ":<alias> ..."; }
 	@Override public String getHelpString() 				{ return "This command views the response <alias>"; }
 	
 }
